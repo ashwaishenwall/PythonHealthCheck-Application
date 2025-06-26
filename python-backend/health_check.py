d228b17e2ef8b90515a9fee175ce77d070a5f1f6
Ashwani shenwall <121819659+ashwaishenwall@users.noreply.github.com>
2025-06-26 13:45:31 +0530

    Update health_check.py

--- python-backend/health_check.py
+++ python-backend/health_check.py
@@ -4,14 +4,9 @@ app = Flask(__name__)
 
 @app.route('/ashwani/shenwall/working')
 def health_check():
-    # Check the health of your application here
-    # You can add more sophisticated checks based on your application's requirements
-
-    # For demonstration purposes, let's assume the application is always healthy
     status = {"status": "OK", "message": "Ashwani everything is fine!"}
-
     return jsonify(status)
 
 if __name__ == '__main__':
-    app.run()
-
+    # Important: listen on all interfaces inside container
+    app.run(host='0.0.0.0', port=5000)
